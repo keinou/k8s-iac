6e9c7ee4f702eee41617892decbe7d2707e862e7
Rafael Karczevski <rkarczevski@gmail.com>
2023-08-11 13:24:12 -0400

    feat: :bug: networking fix

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,7 +5,7 @@ k3s_version=v1.27.4+k3s1
 ansible_user=ubuntu
 systemd_dir=/etc/systemd/system
 master_ip="{{ hostvars[groups['master'][0]]['ansible_host'] | default(groups['master'][0]) }}"
-extra_server_args="--tls-san {{ hostvars[groups['master'][0]]['ansible_host'] | default(groups['master'][0]) }}"
+extra_server_args="--tls-san {{ hostvars[groups['master'][0]]['ansible_host'] | default(groups['master'][0]) }} --disable=traefik"
 extra_agent_args=""
 ansible_ssh_private_key_file="terraform/oci.pem"
 ansible_user=ubuntu
